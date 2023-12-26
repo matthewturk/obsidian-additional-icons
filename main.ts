432c75dca677e83df1079e4f7513e9a7cf2f17c3
Matthew Turk <matthewturk@gmail.com>
2023-12-26 16:28:17 -0600

    Commit settings changes

--- main.ts
+++ main.ts
@@ -29,7 +29,9 @@ export default class AdditionalIconsPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new AdditionalIconsSettingTab(this.app, this));
 
-		await this.addIconsToObsidian(rpgAwesomeSvgStr, "ra");
+		if (this.settings.useRpgAwesome) {
+			await this.addIconsToObsidian(rpgAwesomeSvgStr, "ra");
+		}
 	}
 
 	async addIconsToObsidian(
@@ -41,9 +43,14 @@ export default class AdditionalIconsPlugin extends Plugin {
 		svgDom.querySelectorAll("glyph").forEach((el) => {
 			addIcon(
 				`${prefix}-${el.getAttribute("glyph-name")}`,
-				`<path transform="matrix(0.09765625 0 0 -0.09765625 0, 99.2)" d="${el.getAttribute("d")}"/>`
+				`<path transform="matrix(0.09765625 0 0 -0.09765625 0, 99.2)" d="${el.getAttribute(
+					"d"
+				)}"/>`
 			);
-			console.log("Adding", `${prefix}-${el.getAttribute("glyph-name")} with transform`);
+			console.log(
+				"Adding",
+				`${prefix}-${el.getAttribute("glyph-name")} with transform`
+			);
 		});
 	}
 
@@ -84,15 +91,18 @@ class AdditionalIconsSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("RPG Awesome #1")
+			.setName("RPG Awesome")
 			.setDesc(
 				"Use RPG Awesome Icon Set from https://nagoshiashumari.github.io/Rpg-Awesome/"
 			)
 			.addToggle((toggle) =>
-				toggle.setValue(true).onChange(async (value) => {
-					this.plugin.settings.useRpgAwesmme = value;
-					await this.plugin.saveSettings();
-				})
+				toggle
+					.setValue(this.plugin.settings.useRpgAwesome)
+					.onChange(async (value) => {
+						this.plugin.settings.useRpgAwesome = value;
+						console.log("Setting to ", value);
+						await this.plugin.saveSettings();
+					})
 			);
 	}
 }
