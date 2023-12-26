c7ea9b728770e009f9e63ad7964d81daf4739db2
Matthew Turk <matthewturk@gmail.com>
2023-12-26 15:43:30 -0600

    Import initial icon setting code

--- main.ts
+++ main.ts
@@ -1,89 +1,68 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	getIconIds,
+	addIcon,
+	removeIcon,
+	App,
+	Modal,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import rpgAwesomeSvgStr from "./node_modules/rpg-awesome/fonts/rpgawesome-webfont.svg";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface AdditionalIconsPluginSettings {
+	useRpgAwesome: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: AdditionalIconsPluginSettings = {
+	useRpgAwesome: true,
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AdditionalIconsPlugin extends Plugin {
+	settings: AdditionalIconsPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new AdditionalIconsSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		await this.addIconsToObsidian(rpgAwesomeSvgStr, "ra");
 	}
 
-	onunload() {
+	async addIconsToObsidian(
+		svgFontString: string,
+		prefix: string
+	): Promise<void> {
+		const parser = new DOMParser();
+		const svgDom = parser.parseFromString(svgFontString, "image/svg+xml");
+		svgDom.querySelectorAll("glyph").forEach((el) => {
+			addIcon(
+				`${prefix}-${el.getAttribute("glyph-name")}`,
+				`<path d=${el.getAttribute("d")}/>`
+			);
+			console.log("Adding", `${prefix}-${el.getAttribute("glyph-name")}`);
+		});
+	}
 
+	async onunload() {
+		await this.removePrefixedIcons("ra");
+	}
+
+	async removePrefixedIcons(prefix: string): Promise<void> {
+		for (const iconName of getIconIds()) {
+			if (iconName.startsWith(`${prefix}-`)) removeIcon(iconName);
+		}
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +70,29 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class AdditionalIconsSettingTab extends PluginSettingTab {
+	plugin: AdditionalIconsPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AdditionalIconsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+			.setName("RPG Awesome #1")
+			.setDesc(
+				"Use RPG Awesome Icon Set from https://nagoshiashumari.github.io/Rpg-Awesome/"
+			)
+			.addToggle((toggle) =>
+				toggle.setValue(true).onChange(async (value) => {
+					this.plugin.settings.useRpgAwesmme = value;
 					await this.plugin.saveSettings();
-				}));
+				})
+			);
 	}
 }
